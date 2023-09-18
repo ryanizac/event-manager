0adf4defc28edb854bcce2f95e21137c9a3e8ad6
Ryan Izac <io.ryan@outlook.com>
2023-09-17 22:34:03 -0300

    feat: validate date in create event service

--- src/services/create-event.js
+++ src/services/create-event.js
@@ -20,6 +20,14 @@ export class CreateEventService {
    * @returns {Promise<Result>}
    */
   async execute({ name, date }) {
+    if (typeof name !== "string") {
+      throw new Error("The name must be a string");
+    }
+
+    if (!(date instanceof Date)) {
+      throw new Error("The date must be date instance");
+    }
+
     const createdAt = new Date(),
       updatedAt = createdAt,
       id = Math.random().toString().replace("0.", "");
