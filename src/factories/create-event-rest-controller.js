528f257cebf38bc03e7460080a7db723859d40d7
Ryan Izac <io.ryan@outlook.com>
2023-09-17 22:42:46 -0300

    feat: inject uuid generator into create event service

--- src/factories/create-event-rest-controller.js
+++ src/factories/create-event-rest-controller.js
@@ -1,8 +1,10 @@
 import { CreateEventRestController } from "../controllers/index.js";
 import { CreateEventService } from "../services/index.js";
+import { UUIDGenerator } from "../adapters/index.js";
 
 export function createEventRestControllerFactory() {
-  const service = new CreateEventService();
+  const idGenerator = new UUIDGenerator();
+  const service = new CreateEventService(idGenerator);
   const controller = new CreateEventRestController({
     service,
   });
